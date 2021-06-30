7512b9479272faaaa59a9b1b098fc9ddf16f454e
PARSH JAIN <66243977+immortal-zeus@users.noreply.github.com>
2021-06-30 12:03:43 +0530

    stable

--- scale/suser/views.py
+++ scale/suser/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,"suser/index.html")
+
+def login(request):
+    return render(request,"suser/login.html")
+
+def register(request):
+    return render(request,"suser/register.html")
