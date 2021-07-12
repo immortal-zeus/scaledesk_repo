625d3ff9aff9f219d5b368ddbce6b2c2e9ef1790
PARSH JAIN <66243977+immortal-zeus@users.noreply.github.com>
2021-07-12 14:50:09 +0530

    new checkpoint

--- scale/suser/api/urls.py
+++ scale/suser/api/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
+
    path('',views.UserListView.as_view()),
    path('create',views.Userupdate.as_view()),
    path('<int:pk>',views.Userreti.as_view()),
