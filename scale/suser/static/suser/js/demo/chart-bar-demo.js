a0795d638e4a965d9e7082b9b2a51fabd38000f7
AMAN123956 <amandixit033@gmail.com>
2021-07-13 17:18:22 +0530

    User Profile Page Added

--- scale/suser/static/suser/js/demo/chart-bar-demo.js
+++ scale/suser/static/suser/js/demo/chart-bar-demo.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["15", "20", "March", "April", "May", "June"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "#4e73df",
