067311100cf806fd38d972c8a94504a99d42c3db
PARSH JAIN <66243977+immortal-zeus@users.noreply.github.com>
2021-07-29 21:18:36 +0530

    hotfix-heroku

--- scale/suser/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from suser.models import *
-
-class UserSerializer(serializers.ModelSerializer):
-   class Meta:
-       model = User
-       fields = "__all__"
-
--- scale/suser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SuserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'suser'
